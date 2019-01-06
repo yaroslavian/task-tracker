140bd9382b1ad527ff5fa4c3fdf4eacb1fbd06b3
Yury Romanov <yury.romanov@bk.ru>
2019-01-06 05:52:43 +0300

    Add simple data request handler

--- backend/main.js
+++ backend/main.js
@@ -5,4 +5,22 @@ const PORT = 3000;
 
 app.use(express.static('public'));
 
+app.get('/get-tasks-list', (req, res) => {
+   const dataSet = [
+      { 
+         name: 'Task_1',
+         text: 'Text about task 1'
+      },
+      { 
+         name: 'Task_2',
+         text: 'Text about task 2'
+      },
+      { 
+         name: 'Task_3',
+         text: 'Text about task 3'
+      }
+   ]
+   res.json(JSON.stringify(dataSet));
+});
+
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
