7a9add595990a836b92aec0b56003dd64791891b
Yury Romanov <yury.romanov@bk.ru>
2019-01-08 16:40:44 +0300

    Update build script

--- build.sh
+++ build.sh
@@ -6,3 +6,6 @@ echo '--- Removing old static files from server ---'
 rm ../backend/public/* -rv
 echo '--- Copying new compiled static files to server ---'
 cp dist/* ../backend/public/ -rv
+echo '--- Starting web server ---'
+cd ../backend
+node main.js
