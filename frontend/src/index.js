b2a124aa9f922388c8020d6a936002b399a52d17
Yury Romanov <yury.romanov@bk.ru>
2019-03-24 16:58:33 +0300

    Refactor index.js using useState

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import {Table} from './Components/Table/Table';
 import {Button} from './Components/Button/Button';
@@ -7,51 +7,33 @@ import './style.styl';
 
 const APP_CONTAINER = document.getElementById('app');
 
-class App extends React.Component {
-   constructor(props) {
-      super(props);
+const App = () => {
+   const [currentPage, setPage] = useState('main');
+   let content;
 
-      this.state = {
-         currentPage: 'main'
-      };
+   switch(currentPage) {
+      case 'main':
+         content = (
+            <div>
+               <Button text="Add task" onClick={() => { setPage('add-form'); }} />
+               <Table />
+            </div>
+         );
+         break;
 
-      this.setPage = this.setPage.bind(this);
+      case 'add-form':
+         content = <AddTaskForm setPage={setPage}/>;
+         break;
    }
 
-   setPage(page) {
-      this.setState({
-         currentPage: page
-      });
-   }
-
-   render() {
-      let content;
-
-      switch(this.state.currentPage) {
-         case 'main':
-            content = (
-               <div>
-                  <Button text="Add task" onClick={() => { this.setPage('add-form'); }} />
-                  <Table />
-               </div>
-            );
-            break;
-
-         case 'add-form':
-            content = <AddTaskForm setPage={this.setPage}/>;
-            break;
-         default:
-      }
-
-      return (
-         <div>
-            <h3 className="main-title" onClick={() => { this.setPage('main'); }}>
-               Welcome to the task tracker
-            </h3>
-            {content}
-         </div>
-      );
-   }
-}
+   return (
+      <div>
+         <h3 className="main-title" onClick={() => { setPage('main'); }}>
+            Welcome to the task tracker
+         </h3>
+         {content}
+      </div>
+   );
+};
 
 ReactDOM.render(<App />, APP_CONTAINER);
