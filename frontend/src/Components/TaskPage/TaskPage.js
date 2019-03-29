78694a32f7ce2a89351a10abf0c90c3818c5fe07
Yury Romanov <yury.romanov@bk.ru>
2019-03-29 23:11:38 +0300

    Add some part of edit mode logics for task page

--- frontend/src/Components/TaskPage/TaskPage.js
+++ frontend/src/Components/TaskPage/TaskPage.js
@@ -5,7 +5,7 @@ import './TaskPage.styl';
 
 export const TaskPage = ({match: {params: {id}}}) => {
    const [taskData, setTaskData] = useState(null);
-
+   const [editMode, setEditMode] = useState(false);
 
    useEffect(() => {
       API.getTask(id, (response) => {
@@ -13,16 +13,32 @@ export const TaskPage = ({match: {params: {id}}}) => {
       });
    },[]);
 
+   const handleClick = () => {
+      setEditMode(true);
+   };
+
+
    if(!taskData) {
       return <div></div>;
    }
 
    const {title, text} = taskData;
 
+   if(editMode) {
+      return(
+         <div className="taskPage edit-mode">
+            <input className="taskPage-title" value={title} /><br />
+            <input className="taskPage-text" value={text} />
+            <div className="taskPage-button">Save</div>
+         </div>
+      );
+   }
+
    return (
       <div className="taskPage">
          <div className="taskPage-title"><h3>{title}</h3></div>
          <div className="taskPage-text">{text}</div>
+         <div className="taskPage-button" onClick={handleClick}>Edit</div>
       </div>
    );
 };
