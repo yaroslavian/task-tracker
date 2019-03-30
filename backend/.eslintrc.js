ae21ccef7ba9e1dba5c2cc1739795e72f35b207e
Yury Romanov <yury.romanov@bk.ru>
2019-03-30 13:40:29 +0300

    Update ESLint config

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,26 +1,36 @@
 module.exports = {
-    "env": {
-        "commonjs": true,
-        "es6": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018
-    },
-    "rules": {
+   "env": {
+      "commonjs": true,
+      "es6": true,
+      "node": true
+   },
+   "extends": "eslint:recommended",
+   "globals": {
+      "Atomics": "readonly",
+      "SharedArrayBuffer": "readonly"
+   },
+   "parserOptions": {
+      "ecmaVersion": 2018
+   },
+   "rules": {
       "quotes": ["error", "single"],
       "semi": ["error", "always"],
+      "curly": "error",
+      "object-curly-spacing": ["error", "never"],
+      "template-curly-spacing": ["error", "never"],
       "no-trailing-spaces": "error",
+      "space-before-blocks": "error",
+      "spaced-comment": ["error", "always"],
+      "space-infix-ops": "error",
+      "arrow-spacing": "error",
       "comma-dangle": ["error", "never"],
       "guard-for-in": "error",
       "camelcase": "error",
       "indent": ["error", 3, { "SwitchCase": 1 }],
+      "no-else-return": "error",
+      "no-empty-function": "error",
       "no-useless-constructor": "error",
+      "no-unneeded-ternary": "error",
       "no-duplicate-imports": "error"
-    }
+   }
 };
--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,37 +1,49 @@
 module.exports = {
-    "env": {
+   "env": {
         "browser": true,
         "es6": true
-    },
-    "extends": [
+   },
+   "extends": [
        "eslint:recommended",
        "plugin:react/recommended"
-    ],
-    "globals": {
+   ],
+   "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
-    },
-    "parser": "babel-eslint",
-    "parserOptions": {
-        "ecmaFeatures": {
+   },
+   "parser": "babel-eslint",
+   "parserOptions": {
+      "ecmaFeatures": {
             "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-       "quotes": ["error", "single"],
-       "semi": ["error", "always"],
-       "no-trailing-spaces": "error",
-       "comma-dangle": ["error", "never"],
-       "guard-for-in": "error",
-       "camelcase": "error",
-       "indent": ["error", 3, { "SwitchCase": 1 }],
-       "jsx-quotes": ["error", "prefer-double"],
-       "no-useless-constructor": "error",
-       "no-duplicate-imports": "error"
-    }
+      },
+      "ecmaVersion": 2018,
+      "sourceType": "module"
+   },
+   "plugins": [
+      "react"
+   ],
+   "rules": {
+      "quotes": ["error", "single"],
+      "semi": ["error", "always"],
+      "curly": "error",
+      "object-curly-spacing": ["error", "never"],
+      "template-curly-spacing": ["error", "never"],
+      "no-trailing-spaces": "error",
+      "space-before-blocks": "error",
+      "spaced-comment": ["error", "always"],
+      "space-infix-ops": "error",
+      "arrow-spacing": "error",
+      "comma-dangle": ["error", "never"],
+      "guard-for-in": "error",
+      "camelcase": "error",
+      "indent": ["error", 3, { "SwitchCase": 1 }],
+      "no-else-return": "error",
+      "no-empty-function": "error",
+      "no-useless-constructor": "error",
+      "no-unneeded-ternary": "error",
+      "no-duplicate-imports": "error",
+      
+      // React special
+      "jsx-quotes": ["error", "prefer-double"]
+   }
 };
